34ffbf480bdd18c652700110f70d726661d234d0
vlj2007 <138813026+vlj2007@users.noreply.github.com>
2024-02-07 01:55:47 +0700

    Delete src/main/resources/scripts423.sql

--- src/main/resources/scripts423.sql
+++
@@ -1,7 +0,0 @@
-SELECT student.name, student.age, faculty.name
-FROM student
-INNER JOIN faculty ON student.faculty_id = faculty.id
-
-SELECT student.name, student.age, avatar.file_path
-FROM student
-INNER JOIN avatar ON student.id = avatar.student_id
