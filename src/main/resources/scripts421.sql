85725e0b1c88b069e8a3c357ec1a4d59839940ff
vlj2007 <138813026+vlj2007@users.noreply.github.com>
2024-02-07 01:55:58 +0700

    Delete src/main/resources/scripts421.sql

--- src/main/resources/scripts421.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE student add constraint age CHECK (age>15);
-
-ALTER  TABLE student ALTER COLUMN name SET NOT NULL;
-ALTER  TABLE student add constraint name_unique UNIQUE(name);
-
-ALTER  TABLE faculty add constraint name_color UNIQUE(color);
-
-ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
